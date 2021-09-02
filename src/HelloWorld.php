303932ac24c728133b23664e59e76150fb4783a0
ABI Technology <voeun.so@abi-technologies.com>
2021-09-02 11:41:03 +0700

    add

---
+++ src/HelloWorld.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 9/2/2021
+ * Time: 10:30 AM
+ */
+namespace HelloWorld;
+
+class HelloWorld
+{
+    public function sum($a, $b){
+        return $a + $b;
+    }
+}
